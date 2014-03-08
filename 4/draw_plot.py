dc531d6ef3b94e4ee479e714c94892ac32de3a21
Alexander Belov <git@belov.by>
2014-03-08 22:36:32 +0300

    small python refactoring

--- 4/draw_plot.py
+++ 4/draw_plot.py
@@ -6,15 +6,12 @@ json_data = open('plot_data.json')
 data = json.load(json_data)
 
 x = np.arange(1.0,6.5,0.5)
-y1 = data[0]
-y2 = data[1]
-y = data[2]
 
 plt.figure()
 plt.xlabel('lambda')
 plt.ylabel('denial probability')
-potk, = plt.plot(x, y, 'g^', label="Potk")
-potk1, = plt.plot(x, y1, 'ro', label="Potk1")
-potk2, = plt.plot(x, y2, 'bs', label="Potk2")
+potk, = plt.plot(x, data[2], 'g^', label="Potk")
+potk1, = plt.plot(x, data[0], 'ro', label="Potk1")
+potk2, = plt.plot(x, data[1], 'bs', label="Potk2")
 plt.legend(loc=2)
 plt.show()
